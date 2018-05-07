524761b018475d417e9ccb8b7742772438ae272b
Cedric Nicoloso <cnicoloso@octo.com>
2018-05-07 11:27:07 +1000

    Add quick debug log at startup

--- server.js
+++ server.js
@@ -4,3 +4,4 @@ const app = require('./src/app')
 const port = process.env.PORT || 3000
 app.listen(port)
 log.info(`Server listening on ${port}...`)
+process.env.LOG_LEVEL === 'debug' && log.debug(`Debug activated`)
