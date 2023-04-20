f964986fc378c72138b9fcb9a7d82ef5d26c5468
Cedric Nicoloso <cedric.nicoloso@gmail.com>
2023-04-20 23:11:58 +0200

    Some bumps

--- src/server.js
+++ src/server.js
@@ -2,6 +2,9 @@ import logger from './logger.js'
 import { app } from './app.js'
 
 const port = process.env.PORT || 3000
+
 app.listen(port)
-logger.info(`Server listening on ${port}...`)
+
+logger.info(`Server listening on port ${port}...`)
+
 process.env.LOG_LEVEL === 'debug' && logger.debug(`Debug activated`)
