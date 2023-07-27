a0f1b10a73944fd830d1da92ad3cfa6404484323
Cedric Nicoloso <cedric.nicoloso@gmail.com>
2023-07-27 09:35:26 +0200

    Review ESLint config for TS

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,17 +3,36 @@ module.exports = {
     es2021: true,
     node: true,
   },
-  extends: ['eslint:recommended'],
+  parser: '@typescript-eslint/parser',
+  extends: ['eslint:recommended', 'plugin:import/typescript'],
+  plugins: ['@typescript-eslint', 'no-only-tests', 'import'],
   parserOptions: {
     ecmaVersion: 'latest',
     sourceType: 'module',
   },
+  settings: {
+    'import/parsers': {
+      '@typescript-eslint/parser': ['.ts'],
+    },
+    'import/resolver': {
+      typescript: {
+        project: './tsconfig.json',
+      },
+    },
+  },
+
   rules: {},
-  "overrides": [{
-    "files": ["*.spec.ts", "*.test.ts"],
-    "globals": {
-      "describe": true,
-      "it": true,
-    }
-  }]
+
+  // Tests
+  overrides: [
+    {
+      files: ['*.spec.ts', '*.test.ts'],
+      env: {
+        mocha: true,
+      },
+      rules: {
+        'no-only-tests/no-only-tests': 'error',
+      },
+    },
+  ],
 }
